fceba7c7829db13a7769f5c7b32050cf989f0ce2
m.dorokhin <mdoroxin@yandex.ru>
2019-11-01 16:44:55 +0300

    Добавлена привязка некоторых значений и обработка клика по кнопки

--- app/src/main/java/com/example/clashroyale/activities/deck/DeckViewModel.java
+++ app/src/main/java/com/example/clashroyale/activities/deck/DeckViewModel.java
@@ -1,5 +1,7 @@
 package com.example.clashroyale.activities.deck;
 
+import androidx.databinding.ObservableBoolean;
+import androidx.databinding.ObservableDouble;
 import androidx.lifecycle.ViewModel;
 
 import com.example.clashroyale.api.Api;
@@ -7,7 +9,17 @@ import com.example.clashroyale.api.Api;
 public class DeckViewModel extends ViewModel {
     private final Api mApi;
 
+    public final ObservableBoolean requested = new ObservableBoolean(false);
+    public final ObservableDouble averageElixir = new ObservableDouble(0);
+
     public DeckViewModel(Api api) {
         mApi = api;
+    }
+
+    public void requestDeck() {
+        if (requested.get())
+            return;
+
+        requested.set(true);
     }
 }
