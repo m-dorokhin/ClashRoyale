33c89dd37c8661c1b5baed80e82b1c65f020e7a9
m.dorokhin <mdoroxin@yandex.ru>
2019-11-05 15:32:55 +0300

    Добавлен CardFlipper и анимация перехода между картами

---
+++ app/src/main/java/com/example/clashroyale/ui/cardFlipper/CardFlipper.java
@@ -0,0 +1,54 @@
+package com.example.clashroyale.ui.cardFlipper;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
+
+import com.example.clashroyale.R;
+
+public class CardFlipper extends ViewFlipper {
+    private float fromPosition;
+
+    @SuppressLint("ClickableViewAccessibility")
+    public CardFlipper(Context context) {
+        super(context);
+        this.setOnTouchListener(this::onTouch);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public CardFlipper(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setOnTouchListener(this::onTouch);
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                fromPosition = event.getX();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                float toPosition = event.getX();
+                if (fromPosition > toPosition) {
+                    this.setInAnimation(AnimationUtils
+                            .loadAnimation(this.getContext(), R.anim.flip_in_right_to_left));
+                    this.setOutAnimation(AnimationUtils
+                            .loadAnimation(this.getContext(), R.anim.flip_out_right_to_left));
+                    this.showNext();
+                } else if (fromPosition < toPosition) {
+                    this.setInAnimation(AnimationUtils
+                            .loadAnimation(this.getContext(), R.anim.flip_in_left_to_right));
+                    this.setOutAnimation(AnimationUtils
+                            .loadAnimation(this.getContext(), R.anim.flip_out_left_to_right));
+                    this.showPrevious();
+                }
+            default:
+                break;
+        }
+        return true;
+    }
+}
